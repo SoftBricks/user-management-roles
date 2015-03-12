4653ae7f6b0fdf7116a0476ba4038a971cf79de8
Lukas Sporrer <lukas.sporrer@softbricks.de>
2015-03-12 08:31:04 +0100

    added docs

--- server/methods.js
+++ server/methods.js
@@ -1,4 +1,10 @@
 Meteor.methods({
+    /**
+     * remove a role
+     * @param String role
+     * @return Boolean
+     *      true = successfully removed role
+     */
     'removeRole': function(role){
         var users = Meteor.users.find({roles: role}).fetch();
         Roles.removeUsersFromRoles(users, role);
